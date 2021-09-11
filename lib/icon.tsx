9c62b65a4b1ef1586debf44ca575d5145c6b150d
China1006 <1414894071@qq.com>
2021-09-11 18:03:27 +0800

    添加css

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './importIcons'
+import './icon.scss'
 
 interface IconProps {
     name: string;
 }
-const Icon:React.FunctionComponent<IconProps> = (props) => {
+
+const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
-        <div>
-            <svg>
-                <use xlinkHref={`#${props.name}`}></use>
-            </svg>
-        </div>
+        <svg className="icon">
+            <use xlinkHref={`#${props.name}`}></use>
+        </svg>
     )
 }
 export default Icon
